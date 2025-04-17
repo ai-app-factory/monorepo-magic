e9784e96a0c3668e14b1b7207915e856bc41a5c2
sukrut29 <sukrutssathaye@gmail.com>
2025-04-17 02:22:32 +0200

    build and fix linting

--- mo-mono/dungeon-adventure/packages/common/types/src/runtime-config.ts
+++ mo-mono/dungeon-adventure/packages/common/types/src/runtime-config.ts
@@ -5,10 +5,7 @@ export interface ICognitoProps {
   userPoolWebClientId: string;
 }
 export type ApiUrl = string;
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-empty-interface
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-empty-interface
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-empty-interface
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-empty-interface
+
 export interface IRuntimeConfig {
   httpApis: {
     GameApi: ApiUrl;
