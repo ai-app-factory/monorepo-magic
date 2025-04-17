1c90e4255839f8ea92574d178b5ae741e583785b
sukrut29 <sukrutssathaye@gmail.com>
2025-04-17 02:47:04 +0200

    router setup

--- mo-mono/dungeon-adventure/packages/game-api/backend/src/router.ts
+++ mo-mono/dungeon-adventure/packages/game-api/backend/src/router.ts
@@ -2,14 +2,27 @@ import {
   awsLambdaRequestHandler,
   CreateAWSLambdaContextOptions,
 } from '@trpc/server/adapters/aws-lambda';
-import { echo } from './procedures/echo.js';
+
 import { t } from './init.js';
 import { APIGatewayProxyEventV2WithIAMAuthorizer } from 'aws-lambda';
+import { queryActions } from './procedures/query-actions.js';
+import { saveAction } from './procedures/save-action.js';
+import { queryGames } from './procedures/query-games.js';
+import { saveGame } from './procedures/save-game.js';
 
 export const router = t.router;
 
 export const appRouter = router({
-  echo,
+  actions: router({
+    query: queryActions,
+    save: saveAction,
+  }),
+  games: router({
+    query: queryGames,
+    save: saveGame,
+  }),
+
+
 });
 
 export const handler = awsLambdaRequestHandler({
