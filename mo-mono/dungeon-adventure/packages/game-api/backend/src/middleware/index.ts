a9d1be379137c1f9ba02a74a304cac8e1d46fa5a
sukrut29 <sukrutssathaye@gmail.com>
2025-04-17 02:45:54 +0200

    add dynamodbContext

--- mo-mono/dungeon-adventure/packages/game-api/backend/src/middleware/index.ts
+++ mo-mono/dungeon-adventure/packages/game-api/backend/src/middleware/index.ts
@@ -3,14 +3,17 @@ import type { APIGatewayProxyEventV2WithIAMAuthorizer } from 'aws-lambda';
 import { ILoggerContext } from './logger.js';
 import { IMetricsContext } from './metrics.js';
 import { ITracerContext } from './tracer.js';
+import { IDynamoDBContext } from './dynamodb.js';
 
 export * from './logger.js';
 export * from './metrics.js';
 export * from './tracer.js';
 export * from './error.js';
+export * from './dynamodb.js';
 
 export type IMiddlewareContext =
   CreateAWSLambdaContextOptions<APIGatewayProxyEventV2WithIAMAuthorizer> &
+    IDynamoDBContext &
     ILoggerContext &
     IMetricsContext &
     ITracerContext;
