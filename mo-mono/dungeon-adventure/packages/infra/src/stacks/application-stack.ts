239a7958bb182a3df7550f284859ebb026011bf6
sukrut29 <sukrutssathaye@gmail.com>
2025-04-17 02:20:12 +0200

    add initial code to application-stack

--- mo-mono/dungeon-adventure/packages/infra/src/stacks/application-stack.ts
+++ mo-mono/dungeon-adventure/packages/infra/src/stacks/application-stack.ts
@@ -1,3 +1,9 @@
+import {
+  GameApi,
+  GameUI,
+  StoryApi,
+  UserIdentity,
+} from ':dungeon-adventure/common-constructs';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
@@ -6,5 +12,16 @@ export class ApplicationStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const userIdentity = new UserIdentity(this, 'UserIdentity');
+    const gameApi = new GameApi(this, 'GameApi');
+    const storyApi = new StoryApi(this, 'StoryApi');
+
+    // grant our authenticated role access to invoke our APIs
+    [storyApi, gameApi].forEach((api) =>
+      api.grantInvokeAccess(userIdentity.identityPool.authenticatedRole),
+    );
+
+    // Ensure this is instantiated last so our runtime-config.json can be automatically configured
+    new GameUI(this, 'GameUI');
   }
 }
