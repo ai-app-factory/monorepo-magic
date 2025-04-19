621e98ae0935cb6e76cc455367ab5267ed0e8bcd
sukrut29 <sukrutssathaye@gmail.com>
2025-04-19 20:49:36 +0200

    update index file

--- mo-mono/ng-mono/libs/ui/material/src/index.ts
+++ mo-mono/ng-mono/libs/ui/material/src/index.ts
@@ -6,8 +6,6 @@ export * from './lib/check-box/check-box.component';
 
 export * from './lib/badge/badge.component';
 
-export * from './lib/button-toggle/button-toggle.component';
-
 export * from './lib/radio-button/radio-button.component';
 
 export * from './lib/chip/chip.component';
