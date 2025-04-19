839a02bb53c32c6da71230f22adfe2467b277ca7
sukrut29 <sukrutssathaye@gmail.com>
2025-04-19 20:33:39 +0200

    gen button-toggle in ui material

--- mo-mono/ng-mono/libs/ui/material/src/index.ts
+++ mo-mono/ng-mono/libs/ui/material/src/index.ts
@@ -5,3 +5,5 @@ export * from './lib/button/button.component';
 export * from './lib/check-box/check-box.component';
 
 export * from './lib/badge/badge.component';
+
+export * from './lib/button-toggle/button-toggle.component';
