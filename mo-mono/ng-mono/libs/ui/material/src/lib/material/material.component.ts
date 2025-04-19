4aca17a8deeb6d6a2050d3a2463e48ec43a732e1
sukrut29 <sukrutssathaye@gmail.com>
2025-04-19 15:24:57 +0200

    generate material ui lib in ng-mono

---
+++ mo-mono/ng-mono/libs/ui/material/src/lib/material/material.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ui-mat-material',
+  imports: [CommonModule],
+  templateUrl: './material.component.html',
+  styleUrl: './material.component.scss',
+})
+export class MaterialComponent {}
