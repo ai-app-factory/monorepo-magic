e56928d0b9089fffda4a38826abc3b081c80ca5c
sukrut29 <sukrutssathaye@gmail.com>
2025-04-19 15:18:29 +0200

    add exports to index file for the interfaces already implemented

--- mo-mono/libs/atomic-design/src/index.ts
+++ mo-mono/libs/atomic-design/src/index.ts
@@ -1,1 +1,6 @@
 export * from './lib/atomic-design.js';
+export * from './lib/atoms/AtomicButton.js';
+export * from './lib/atoms/AtomicInput.js';
+export * from './lib/atoms/AtomicLabel.js';
+export * from './lib/atoms/AtomicPI.js'
+export * from './lib/molecules/MolecularSearch.js'
