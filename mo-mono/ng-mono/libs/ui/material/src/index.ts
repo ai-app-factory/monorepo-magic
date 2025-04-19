034d683726db4ce2bcd6bfb8e1fad324724de991
sukrut29 <sukrutssathaye@gmail.com>
2025-04-19 20:28:24 +0200

    gen check-box in ui/material

--- mo-mono/ng-mono/libs/ui/material/src/index.ts
+++ mo-mono/ng-mono/libs/ui/material/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/material/material.component';
 
 export * from './lib/button/button.component';
+
+export * from './lib/check-box/check-box.component';
---
+++ mo-mono/ng-mono/libs/ui/material/src/lib/check-box/check-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ui-mat-check-box',
+  imports: [CommonModule],
+  templateUrl: './check-box.component.html',
+  styleUrl: './check-box.component.scss',
+})
+export class CheckBoxComponent {}
